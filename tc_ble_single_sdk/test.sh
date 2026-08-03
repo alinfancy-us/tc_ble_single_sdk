407573ce0e4bf77af22996f5b5b0a754f43f695d
beyondboy <xuguoli.scau@gmail.com>
2026-08-03 17:38:53 +0800

    remove unused files and recover officail files

--- tc_ble_single_sdk/test.sh
+++
@@ -1,7 +0,0 @@
-# 1. 设置 Homebrew 核心镜像（清华大学源）
-export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
-export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
-
-# 2. 设置瓶装预编译包（API/Bottles）镜像
-export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
-export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
